e413653d7744ddc0025f51f981de4f69834bf3cb
navazsayani <navaz.sayani@somaiya.edu>
2018-04-07 16:08:59 +0530

    defined value of a

--- reverse.py
+++ reverse.py
@@ -1,5 +1,6 @@
-#NAVAZ First Comment -- new one
+#NAVAZ First Comment -- new one two three
 #Second comment
 a=str(input("Enter a string: "))
 print("Reverse of the string is: ")
 print(a[::-1])
+#a = 99
